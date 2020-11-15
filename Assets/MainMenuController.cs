fa364cddba6541ea27bdb5db52b00619de3639b6
Unknown <rkay94@gmail.com>
2020-11-15 07:24:49 -0500

    menu and end screen

---
+++ Assets/MainMenuController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/ReturnToMainMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnToMainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ControlFreak.Controls.Player.Suicide.performed += Suicide_performed;
+    }
+
+    private void Suicide_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        SceneManager.LoadScene(0);
+    }
+}
